dfc6261c9d217ae373e1b101dc84a18324327a60
ZhangXuzhe <ZhangXuzhe@DESKTOP-J04D9FM>
2019-01-15 00:10:10 +0800

    Mybaties接口式

--- src/com/haut/Factory/MybatiesTest.java
+++ src/com/haut/Factory/MybatiesTest.java
@@ -9,35 +9,57 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.haut.bean.GoodsList;
+import com.haut.dao.Goods;
 
 public class MybatiesTest {
 	public static void main(String[] args) {
 	 try {
-		GoodsList sql = new MybatiesTest().test();
-		System.out.println(sql.getGid()+sql.getGname());
-	} catch (IOException e) {
+		/*GoodsList sql = new MybatiesTest().test();*/
+		 GoodsList jieKou = new MybatiesTest().jieKou();
+		System.out.println(jieKou.getGid()+jieKou.getGname());
+	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	}
+	
+	public SqlSession sqlSession() throws IOException{
+		Reader resourceAsReader = Resources.getResourceAsReader("mybaties.xml");
+		SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(resourceAsReader);
+		//获取SqlSession实例
+		SqlSession openSession = sqlSessionFactory.openSession();
+		return openSession;
 	}
 	
 	public  GoodsList test() throws IOException{
 		//根据xml配置文件(全局配置文件)创建一个SqlSessionFactory对象
 /*		String resource="mybatiesmapper.xml";
 		InputStream inputStream=Resources.getResourceAsStream(resource);*/
-		Reader resourceAsReader = Resources.getResourceAsReader("mybaties.xml");
+		
+		
+/*		Reader resourceAsReader = Resources.getResourceAsReader("mybaties.xml");
 		SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(resourceAsReader);
 		//获取SqlSession实例
-		SqlSession openSession = sqlSessionFactory.openSession();
-		GoodsList one = openSession.selectOne("com.haut.mybaties.mybatiesMapper.selectGoods", 2);
+		SqlSession openSession = sqlSessionFactory.openSession();*/
 		
+		GoodsList one = sqlSession().selectOne("com.haut.mybaties.mybatiesMapper.selectGoods", 2);
 		
 		//查询结束关闭会话
-		openSession.close();
-		return one;
+		sqlSession().close();
+		return one;	
+	}
+	
+	//接口式编程
+	public GoodsList jieKou() throws IOException{
+			SqlSession sqlSession = sqlSession();
+			//使用getMapper(接口名.class) 创建一个代理对象
+			Goods mapper = sqlSession.getMapper(Goods.class);
+			//代理对象.mapper sql映射文件XXXMapper.xml文件的id
+			GoodsList selGoods = mapper.selGoods(3);
+			sqlSession.close();
+			return selGoods;
 	
 
-	
 		
 	}
 }
